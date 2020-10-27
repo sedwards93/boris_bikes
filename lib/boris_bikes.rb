0b5807067beee5f48ab7ecf4316e82c5c07dae98
Alex Withington-Smith <alex@awithsmith.com>
2020-10-27 15:38:35 +0000

    passing initial test for #release_bike

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -1,3 +1,21 @@
 class DockingStation
+  attr_reader :bikes
 
+  def initialize(first_bikes = [])
+    @bikes = first_bikes
+  end
+
+  def release_bike(id)
+    @bikes.each_with_index do |bike, index|
+      @bikes.delete_at(index) if bike.id == id
+    end
+  end
+end
+
+class Bike
+  attr_reader :id
+
+  def initialize(id)
+    @id = id
+  end
 end
