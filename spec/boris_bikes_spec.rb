6ed1f9e9839b2acc04116387998b69c86b843142
Symion Edwards <symion.edwards@gmail.com>
2020-10-27 15:24:15 +0000

    Added rspec expectation for release_bike

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -4,4 +4,12 @@ describe DockingStation do
   it "exists" do
     test = DockingStation.new
   end
+describe "#release_bike" do
+  it "removes bike of matching ID from list of bikes" do
+    brixton = DockingStation.new([Bike.new(1)])
+    brixton.release_bike(1)
+    expect(brixton.bikes).to eq([])
+  end
 end
+end
+
