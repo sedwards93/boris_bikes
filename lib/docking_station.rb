8e02c49c9fce17a61db360cad9e33f0681eec9ed
Symion Edwards <symion.edwards@gmail.com>
2020-11-01 17:45:14 +0000

    Refactored to conform with single responsibility principle

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,13 +8,22 @@ class DockingStation
     end
 
     def dock(bike) 
-        raise "This station already is at capacity" if @bikes.count >= 20
+        raise "This station already is at capacity" if full?
         @bikes << bike
     end
 
     def release_bike
-        raise "There are no bikes" if @bikes.empty?
+        raise "There are no bikes" if empty?
         @bikes.pop
        end
+    
+    private
 
+    def full?
+        @bikes.count >= 20
+    end
+
+    def empty?
+        @bikes.empty?
+    end      
 end
